5cafcd47db53a86de165e6279bf6f364b503ea66
kakacarlos2324 <carlosabreu20340@gmail.com>
2023-08-06 20:31:05 -0300

    first commit

---
+++ tarefa.py
@@ -0,0 +1,11 @@
+nome = ("Carlos Gabriel Abreu Rocha")
+idade = 18
+altura = 1.76
+peso = 80
+maior_de_idade = idade >= 18
+
+print("nome", nome)
+print("idade", idade, "anos")
+print("altura", altura, "metros")
+print("peso", peso, "kg")
+print("maior_de_idade", "sim" if maior_de_idade else "não")
